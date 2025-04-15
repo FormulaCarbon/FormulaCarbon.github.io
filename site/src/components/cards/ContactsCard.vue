c472b2203df60da22305727e7f3d9815a7a9073d
FormulaCarbon <99456163+FormulaCarbon@users.noreply.github.com>
2025-04-14 20:38:06 -0500

    email

--- site/src/components/cards/ContactsCard.vue
+++ site/src/components/cards/ContactsCard.vue
@@ -1,44 +1,33 @@
 <script setup>
-function copyEmail() {
-    navigator.clipboard.writeText("siddharth.kakumanu@gmail.com");
-    alert("Email copied to clipboard!");
-}
-
 const links = [
-    { icon: "bi bi-github", url: "https://github.com/FormulaCarbon" },
-    { icon: "bi bi-reddit", url: "https://reddit.com" },
-    { icon: "bi bi-linkedin", url: "https://linkedin.com" },
-    { icon: "bi bi-envelope-fill", url: "email" },
+    { icon: "bi bi-github", url: "https://github.com", text: "" },
+    { icon: "bi bi-reddit", url: "https://reddit.com", text: "s" },
+    { icon: "bi bi-linkedin", url: "https://linkedin.com", text: "" },
 ];
 </script>
 
 <template>
-    <div class="icon-links">
-        <div v-for="(link, index) in links" :key="index" :href="link.url">
-            <div
-                v-if="link.url === 'email'"
-                @click="copyEmail()"
-                target="_blank"
-                rel="noopener"
-                class="icon-email"
-            >
-                <i :class="link.icon"></i>
+    <div class="container">
+        <div class="icon-links">
+            <div v-for="(link, index) in links" :key="index" class="icon-link">
+                <a :href="link.url" target="_blank" rel="noopener noreferrer">
+                    <i :class="link.icon"></i>
+                </a>
             </div>
-
-            <a
-                v-else-if="link.url !== 'email'"
-                :href="link.url"
-                target="_blank"
-                rel="noopener"
-                class="icon-link"
-            >
-                <i :class="link.icon"></i>
-            </a>
         </div>
+        siddharth.kakumanu@gmail.com
     </div>
 </template>
 
 <style scoped>
+.container {
+    display: flex;
+    flex-direction: column;
+    gap: 0.5rem;
+    justify-content: center;
+    align-items: center;
+    height: 100%;
+}
 .icon-links {
     display: flex;
     gap: 2rem;
@@ -54,18 +43,7 @@ const links = [
     color: var(--color-light);
     transition: color 0.3s;
 }
-.icon-email i {
-    display: inline-block;
-    vertical-align: middle;
-    line-height: 1;
-    font-size: 5.25rem;
-    color: var(--color-light);
-    transition: color 0.3s;
-}
 .icon-link:hover i {
-    color: var(--color-accent);
-}
-.icon-email:hover i {
     color: var(--color-accent);
 }
 </style>
